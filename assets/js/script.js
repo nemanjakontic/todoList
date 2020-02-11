6432582dec479371ecc1c26ea1fdb4a799d7a274
nemanjakontic <nemanja.kontic28@gmail.com>
2020-02-11 16:48:29 +0100

    deleting todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $("li").on("click", function () {
     $(this).toggleClass("completed");
 });
+
+$("span").on("click", function (event) {
+    $(this).parent().fadeOut(500, function () {
+       $(this).remove(); 
+    });
+    event.stopPropagation();
+});
